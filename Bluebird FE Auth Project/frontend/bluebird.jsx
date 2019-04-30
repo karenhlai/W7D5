3a5519b481dffc3c4622a4d6e7fd3cb78b06942b
Karen <laikarenh@gmail.com>
2019-04-30 14:08:32 -0700

    Check current_user in application.html to used instead of _nullsession

--- Bluebird FE Auth Project/frontend/bluebird.jsx
+++ Bluebird FE Auth Project/frontend/bluebird.jsx
@@ -6,13 +6,17 @@ import Root from './components/root';
 document.addEventListener('DOMContentLoaded', () => {
   const root = document.getElementById('root');
   let preloadedState = undefined;
+
+  //preloaded is passed in instead of _nullsession in reducer
   if (window.currentUser) {
     preloadedState = {
       session: {
-        currentUser: window.currentUser
+        currentUser: window.currentUser //POJO with cU id, un, and email
       }
     };
   }
+
+
   const store = createStore(preloadedState);
   // const store = createStore();
 
